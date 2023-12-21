3eeebaef85eadd9f52e7d50c258a9abc703cf70f
“nfdiop” <ndeye-fatou.diop@berkeley.edu>
2023-12-21 12:22:10 +0100

    render cells

--- src/components/Guesses.js
+++ src/components/Guesses.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { range } from "../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../constants"
 
 export function Guesses({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map(({ label, id}) => <p class="guess" key={id}>{label}</p>)}
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((idx) => {
+        const maybeGuess = guesses[idx];
+        const renderedGuess =
+          maybeGuess == null ? (
+            <EmptyGuess />
+          ) : (
+            <GuessedWord guess={maybeGuess} />
+          );
+        return <React.Fragment key={idx}>{renderedGuess}</React.Fragment>;
+      })}
     </div>
+  );
+}
+
+function EmptyGuess() {
+  return (
+    <p className="guess">
+      <span className="cell"></span>
+      <span className="cell"></span>
+      <span className="cell"></span>
+      <span className="cell"></span>
+      <span className="cell"></span>
+    </p>
+  );
+}
+
+function GuessedWord({ guess }) {
+  const { label } = guess;
+  return (
+    <p className="guess">
+      <span className="cell">{label[0]}</span>
+      <span className="cell">{label[1]}</span>
+      <span className="cell">{label[2]}</span>
+      <span className="cell">{label[3]}</span>
+      <span className="cell">{label[4]}</span>
+    </p>
   );
 }
